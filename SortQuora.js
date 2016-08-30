36b5f9c10dde1f5aaf7132cfa891211ce6b04aad
AlessandroFC15 <ale-remo@hotmail.com>
2016-08-29 21:45:47 -0300

    Refactoring code

--- SortQuora.js
+++ SortQuora.js
@@ -1,35 +1,50 @@
-var potentiallyAnswers = $('.pagedlist_item');
+var getAnswersDivs = function(classContainer, individualClassContainer) {
+    var potentiallyAnswers = $(classContainer);
 
-var answersDivs = [];
+    var answersDivs = [];
 
-for (var i = 0; i < potentiallyAnswers.length; i++) {
-	var answer = $('#' + potentiallyAnswers[i].id).find('.AnswerBase');
+    for (var i = 0; i < potentiallyAnswers.length; i++) {
+        var answer = $('#' + potentiallyAnswers[i].id).find(individualClassContainer);
 
-	if (answer.length > 0) {
-		answersDivs.push(potentiallyAnswers[i])
-	}
+        if (answer.length > 0) answersDivs.push(potentiallyAnswers[i]);
+    }
+
+    return answersDivs;
+};
+
+var getNumberOfUpvotesPerAnswer = function(answersDivs) {
+    var data = []
+
+    for (var i = 0; i < answersDivs.length; i++) {
+        var countDiv = $('#' + answersDivs[i].id).find('.answer_upvote');
+
+        var count = $(countDiv[0].children[1])[0]
+
+        data.push({ valor: Number($(count).html()), indice: i});
+    }
+
+    data.sort(function(a, b) {
+      return b.valor - a.valor;
+    });
+
+    return data;
+};
+
+var updateQuoraPage = function(data, mainContainerClass, answersDivs) {
+    var mainContainer = $(mainContainerClass);
+
+    mainContainer.empty();
+
+    for (var i = 0; i < data.length; i++) {
+        mainContainer.append(answersDivs[data[i].indice]);
+    }
 }
 
-var data = []
+/* -- MAIN -- */
 
-for (var i = 0; i < answersDivs.length; i++) {
-	var countDiv = $('#' + answersDivs[i].id).find('.answer_upvote');
+var answersDivs = getAnswersDivs('.pagedlist_item', '.AnswerBase');
 
-	var count = $(countDiv[0].children[1])[0]
+var data = getNumberOfUpvotesPerAnswer(answersDivs);
 
-	var countValue = $(count).html();
+updateQuoraPage(data, '.AnswerPagedList', answersDivs);
 
-	data.push({ valor: Number(countValue), indice: i});
-}
-
-data.sort(function(a, b) {
-  return b.valor - a.valor;
-});
-
-var mainContainer = $('.AnswerPagedList');
-
-mainContainer.empty();
-
-for (var i = 0; i < data.length; i++) {
-	mainContainer.append(answersDivs[data[i].indice]);
-}
